f50693ceaea12edf8820718fefe76334a1a4f17d
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-02-19 20:56:31 +0530

    2 files modified

--- img/main1.js
+++ img/main1.js
@@ -1,1 +1,21 @@
-console.log("ello")
+var fileTag = document.getElementById("filetag"),
+    preview = document.getElementById("preview");
+    
+fileTag.addEventListener("change", function() {
+  changeImage(this);
+});
+
+function changeImage(input) {
+  var reader;
+
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+      preview.setAttribute('src', e.target.result);
+      console.log(e.target.result)
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
